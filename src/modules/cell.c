ed501b9eb5dceed79bbd3825ce837e941308bbac
Maxime CERRUTI <104192339+Wedokia@users.noreply.github.com>
2023-12-10 16:17:46 +0100

    Update cell.c

--- src/modules/cell.c
+++ src/modules/cell.c
@@ -1,19 +1,35 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "../../includes/cell.h"
+#include "include/cell.h"
 
-t_d_cell *createCell(int value, int levels) {
-    t_d_cell *cell = (t_d_cell *)calloc(1, 8);
-    cell->value = value;
-    cell->levels = levels;
-    cell->next = (t_d_cell**)calloc(levels+1, 8);
-    for (int i = 0; i < levels; ++i) {
-        cell->next[i] = NULL;
+Cell *createCell(int cellValue, int cellLevel) {
+    Cell *cell = (Cell *)calloc(1, SIZE_OF_CELL);
+    if (cell == NULL) return NULL; // gestion de l'échec de l'allocation
+
+    *cell = (Cell){cellValue, cellLevel, (Cell **)calloc(cellLevel + 1, SIZE_OF_PTR)};
+    if (cell->next == NULL) {
+        free(cell); return NULL; // gestion de l'échec de l'allocation
     }
+
+    // inits all pointers to NULL
+    memset(cell->next, 0, (cellLevel + 1) * SIZE_OF_PTR); 
     return cell;
 }
 
-void freeCell(t_d_cell *cell) {
-    free(cell->next);
-    free(cell);
+inline void freeCell(Cell *cell) {
+    if (cell != NULL) {
+        free(cell->next);
+        cell->next = NULL;  // Mettre à jour les pointeurs à NULL après la libération
+        free(cell);
+    }
+}
+
+void displayCell(Cell *cell) {
+    printf("[CELL | %p]  Value: %d, Level: %d, Next: [", cell, cell->value, cell->level);
+    for (int i = 0; i <= cell->level; ++i) {
+        printf("%p%s", (void *)(cell->next[i]), (i < cell->level) ? ", " : "");
+    }
+    printf("]\n");
 }
